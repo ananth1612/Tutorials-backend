d6d79796481a6bcb0ddb7df71a03d0b02317545f
ananth1612 <36665835+ananth1612@users.noreply.github.com>
2018-11-07 00:50:26 +0530

    Add files via upload

---
+++ src/main/java/com/javawithaws/tutorials/javawithaws/JavawithawsApplication.java
@@ -0,0 +1,12 @@
+package com.javawithaws.tutorials.javawithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawithawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawithawsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javawithaws/tutorials/javawithaws/JavawithawsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javawithaws.tutorials.javawithaws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavawithawsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
